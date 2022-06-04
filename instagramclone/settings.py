f50135fe9fd886cf0b4dcd2d0cef837b16885ffb
maureen-james <manjax97@gmail.com>
2022-06-05 01:32:00 +0300

    Rebug:modify settings file

--- instagramclone/settings.py
+++ instagramclone/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'instagram'
-    'bootstrap4'
+    'instagram',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
