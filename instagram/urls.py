8b585e368cebdf89c4610fe53d8be4397b6b054e
maureen-james <manjax97@gmail.com>
2022-06-05 00:32:20 +0300

    Register apthe app in installed apps

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,4 @@
-from django import views
+from . import views
 from django.urls import path
 
 
--- instagram/views.py
+++ instagram/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
+
     return render(request, 'welcome.html')
     
