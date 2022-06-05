680f26398cedacb5261803f754a0eb08ae7afa64
maureen-james <manjax97@gmail.com>
2022-06-05 05:10:24 +0300

    feat:change url patterns

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,24 @@
+# from . import views
+# from django.urls import path
+
+
+
+# urlpatterns=[
+#     path('',views.welcome,name = 'welcome'),
+# ]
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
-from django.urls import path
-
-
 
 urlpatterns=[
-    path('',views.welcome,name = 'welcome'),
+    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^accounts/profile/$',views.profile,name = 'profile'),
+    url(r'^profile/(\d+)',views.other_profile,name = 'visitprofile'),
+    url(r'^search/profile$', views.search, name='profileresults'),
+    url(r'^timeline$', views.timeline, name='timeline'),
+    url(r'^edit_profile$', views.edit_profile, name='edit'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
