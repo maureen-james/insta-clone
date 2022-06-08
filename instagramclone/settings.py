f969129754f4d45a7538bd85848245a99e79c3b1
maureen-james <manjax97@gmail.com>
2022-06-09 02:56:49 +0300

    modify settings file on database

--- instagramclone/settings.py
+++ instagramclone/settings.py
@@ -95,20 +95,14 @@ WSGI_APPLICATION = 'instagramclone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASE_URL =  'postgresql://tlxjbzxhzkkrjq:08388b5f7213fe3782c17bbb1f51f1d2a9ec4b2a85cd037c95b7332178b6e579@ec2-54-165-178-178.compute-1.amazonaws.com:5432/deqnhqbnnh9ubp'
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default = config('DATABASE_URL')
-#     )
-# } 
+# DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+DATABASES = {
+    'default': dj_database_url.config(
+        default = config('DATABASE_URL')
+    )
+} 
 
 
 
