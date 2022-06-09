239cc96075f9e6caf2e9a9883e0280e73e38e451
maureen-james <manjax97@gmail.com>
2022-06-09 09:26:57 +0300

    final deployment

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from .models import Profile,Post,Comment,Following
+
+class FollowingTestClass(TestCase):
+    def setUp(self):
+        self.username=Following(username='maureen',followed='john')
+                            
+    def test_instance(self):
+        self.assertTrue(isinstance(self.esther,Following))
+
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.first=Comment(post=1,
+                            username='maureen',
+                            comment='Epic performance',
+                            date='May 24, 2019, 12:41 a.m.',
+                            count=0)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.first,Comment))
